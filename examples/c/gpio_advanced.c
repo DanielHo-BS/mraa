8fcff15eaddb375971f26bb012f913025b1bcfe8
DanielHo-BS <danielho1671@gmail.com>
2022-03-31 10:10:13 +0800

    Add ISR Usage

--- examples/c/gpio_advanced.c
+++ examples/c/gpio_advanced.c
@@ -16,54 +16,149 @@
 
 /* mraa header */
 #include "mraa/gpio.h"
+#include "mraa/led.h"
 
-#define GPIO_PIN 6
+/* GPIO default */
+#define GPIO_PIN1 5
+#define GPIO_PIN2 6
 
-void
-int_handler(void* args)
-{
+/* LED default value */
+#define LED_NUM1   0
+#define LED_NUM2   1
+#define LED_ON_OFF 0
+
+/* trigger type */
+#define LED_TRIGGER "heartbeat"
+
+volatile int isr_occurred;
+
+void int_handler() {
     fprintf(stdout, "ISR triggered\n");
+    isr_occurred = 1;
 }
 
-int
-main()
-{
+void isr_do(mraa_led_context led1, mraa_led_context led2, int led_on_off, int hz, int wait_idle){
+    for(int i =0; i < hz; i++) {
+        /* turn LED on/off depending on max_brightness value */
+        mraa_led_set_brightness(led1, led_on_off);
+        mraa_led_set_brightness(led2, led_on_off);
+        usleep(wait_idle);
+        led_on_off = !led_on_off;
+    }
+}
+
+
+int main() {
     mraa_result_t status = MRAA_SUCCESS;
-    mraa_gpio_context gpio;
+    mraa_gpio_context gpio, gpio_2;
+    mraa_led_context led1, led2;
+    int led_num1, led_num2, led_on_off;
+    led_num1 = LED_NUM1;
+    led_num2 = LED_NUM2;
+    led_on_off = LED_ON_OFF;
 
     /* initialize mraa for the platform (not needed most of the times) */
     mraa_init();
 
     //! [Interesting]
     /* initialize GPIO pin */
-    gpio = mraa_gpio_init(GPIO_PIN);
+    gpio = mraa_gpio_init(GPIO_PIN1);
+    /* Wait for the GPIO to initialize*/
+    usleep(5000);
     if (gpio == NULL) {
-        fprintf(stderr, "Failed to initialize GPIO %d\n", GPIO_PIN);
+        fprintf(stderr, "Failed to initialize GPIO %d\n", GPIO_PIN1);
         mraa_deinit();
         return EXIT_FAILURE;
     }
 
-    /* set GPIO to input */
+    gpio_2 = mraa_gpio_init(GPIO_PIN2);
+    /* Wait for the GPIO to initialize*/
+    usleep(5000);
+    if (gpio_2 == NULL) {
+        fprintf(stderr, "Failed to initialize GPIO %d\n", GPIO_PIN2);
+        mraa_deinit();
+        return EXIT_FAILURE;
+    }
+    /* initialize LED */
+    led1 = mraa_led_init(led_num1);
+    usleep(5000);;
+    if (led1 == NULL) {
+        fprintf(stderr, "Failed to initialize LED 1\n");
+        mraa_deinit();
+        return EXIT_FAILURE;
+    }    
+    led2 = mraa_led_init(led_num2);
+    usleep(5000);
+    if (led2 == NULL) {
+        fprintf(stderr, "Failed to initialize LED 2\n");
+        mraa_deinit();
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stdout,"Success to initialize LED and GPIO!\n");
+
+    /* set GPIO1 to input */
     status = mraa_gpio_dir(gpio, MRAA_GPIO_IN);
+    usleep(5000);
     if (status != MRAA_SUCCESS) {
         goto err_exit;
     }
+    /* set GPIO2 to output */
+    status = mraa_gpio_dir(gpio_2, MRAA_GPIO_OUT);
+    usleep(5000);
+    if (status != MRAA_SUCCESS) {
+        goto err_exit;
+    }
+    fprintf(stdout,"Success to set dir!\n");
 
     /* configure ISR for GPIO */
-    status = mraa_gpio_isr(gpio, MRAA_GPIO_EDGE_BOTH, &int_handler, NULL);
+    status = mraa_gpio_isr(gpio, MRAA_GPIO_EDGE_RISING, &int_handler, NULL);
+    /* Wait for the ISR to configure*/
+    usleep(5000);
     if (status != MRAA_SUCCESS) {
         goto err_exit;
     }
+    fprintf(stdout,"Success to configure ISR!\n");
 
-    /* wait 30 seconds isr trigger */
-    sleep(30);
-
+    
+    int c = 0;
+    int hz = 10;
+    int min_fsync_interval = 5000;
+    int wait_idle = (1000000-min_fsync_interval)/hz;
+    while(c < 20) { // tirger 20 times
+        status = mraa_gpio_write(gpio_2, 1);
+            if (status != MRAA_SUCCESS) {
+                goto err_exit;
+            }
+        usleep(min_fsync_interval);
+        status = mraa_gpio_write(gpio_2, 0);
+            if (status != MRAA_SUCCESS) {
+                goto err_exit;
+            }
+        
+        if (isr_occurred){
+                isr_occurred = 0;
+                isr_do(led1, led2, led_on_off, hz, wait_idle);
+        }
+        c = c + 1;
+    }
+    /* turn off LED*/
+    mraa_led_set_brightness(led1, 0);
+    mraa_led_set_brightness(led2, 0);
+    usleep(5000);
     /* close GPIO */
     mraa_gpio_close(gpio);
+    mraa_gpio_close(gpio_2);
+
+    /* close LED */
+    mraa_led_close(led1);
+    mraa_led_close(led2);
+
 
     //! [Interesting]
     /* deinitialize mraa for the platform (not needed most of the times) */
     mraa_deinit();
+    sleep(1);
 
     return EXIT_SUCCESS;
 
